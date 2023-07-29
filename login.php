39a83ded7295290efa7347e3c1681718d442b7dd
Vaibhavpan02 <pandeyvaibhav513@gmail.com>
2023-07-29 22:01:32 +0530

    Eduflix-Educational Website (How Technology Changing Education)

---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+   $email= $_POST['email'];
+   $password=$_POST['password'];
+
+   $con =new mysqli("localhost","root","test"):
+   if($con->connect_error){
+       die("Failed to connect : ".$con->connect_error):
+   }
+   else{
+       $stmt =$con->prepare("select * from registration where email =?");
+       $stmt_>blind_param("s",$email);
+       $stmt->execute();
+       $stmt_result=$stmt->get_result();
+       if($stmt_result->num_rows>0){
+           $data + $stmt_result->fetch_assoc();
+           if($data['password']== $password){
+               echo "<h2>Login successfully</h2>"
+           }
+        }else{
+            echo "<h2>Invalid Email or Password</h2>"
+        }
+    }else{
+        echo "<h2>Invalid Email or Password</h2>"
+    }
+           }
+       }
+   
